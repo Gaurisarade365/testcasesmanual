55d7aa7d79717a7af8b3c85ee16fe2b5b7c51ed4
Gaurisarade365 <gaurisarade7662@gmail.com>
2025-10-30 16:02:54 +0530

    Create Loginpage.java

---
+++ Loginpage.java
@@ -0,0 +1,36 @@
+package pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class LoginPage {
+    private WebDriver driver;
+
+    // Locators
+    private By usernameField = By.name("username");
+    private By passwordField = By.name("password");
+    private By loginButton = By.xpath("//button[@type='submit']");
+    private By dashboardHeader = By.xpath("//h6[text()='Dashboard']");
+
+    // Constructor
+    public LoginPage(WebDriver driver) {
+        this.driver = driver;
+    }
+
+    // Actions
+    public void enterUsername(String username) {
+        driver.findElement(usernameField).sendKeys(username);
+    }
+
+    public void enterPassword(String password) {
+        driver.findElement(passwordField).sendKeys(password);
+    }
+
+    public void clickLogin() {
+        driver.findElement(loginButton).click();
+    }
+
+    public boolean isDashboardVisible() {
+        return driver.findElement(dashboardHeader).isDisplayed();
+    }
+}
